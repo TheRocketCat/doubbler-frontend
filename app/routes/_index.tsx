c380bb751e394843a91196e42b16c288da926345
albin brånebäck <dakondo@protonmail.com>
2024-03-24 22:45:15 +0100

    login

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import { type MetaFunction,json } from "@remix-run/node";
+import { type MetaFunction,json,LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
@@ -8,30 +8,57 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export async function loader() {
-	const res= await fetch('http://localhost:3003/store/offers')
-	return json(await res.json())
+export async function loader({request}:LoaderFunctionArgs) {
+	const cookie= request.headers.get("Cookie")
+	const auth= cookie?.split(";").find((c) => c.trim().startsWith("access_token=")).split("=")[1]
+
+	const res= await fetch('http://localhost:3003/store/offers',{
+		headers: {
+			"Authorization": "Bearer " + auth
+		}
+	})
+	const doubl= await fetch('http://localhost:3003/user/doubloner',{
+		headers: {
+			"Authorization": "Bearer " + auth
+		}
+	})
+	if(!res.ok){
+		throw new Error("Failed to fetch offers")
+	}
+	if(!doubl.ok){
+		console.log(doubl)
+		throw new Error("Failed to fetch doubloner")
+	}
+
+	return json({
+		offers: await res.json(),
+		doubloner: (await doubl.json()).doubloner
+	})
 }
 
 export default function Index() {
 	const json = useLoaderData<typeof loader>()
-	console.log(json)
 
+	console.log(json)
   return (
-	<>
-		<h1>Offers</h1> 
-		{json.map((offer) => (
-			<div style={{"margin":"10px","backgroundColor":"blue","padding":"10px"}}>
-				<h1>{offer._name}</h1>
-				<h2>Price: {offer._doublonerPrice}</h2>
-				<button>Buy</button>
-			</div>
-		) )}
-	</>
+	<div>
+		<p>doubloner: {json.doubloner}</p>
+		<div>
+			<h1>Offers</h1> 
+			{json.offers.map((offer) => (
+				<div style={{"margin":"10px","backgroundColor":"blue","padding":"10px"}}>
+					<h1>{offer._name}</h1>
+					<h2>Price: {offer._doublonerPrice}</h2>
+					<button onClick={()=>buy(offer.id)}>Buy</button>
+				</div>
+			) )}
+		</div>
+	</div>
   );
 }
 
-function Buy(){
+function buy(name){
+	console.log(name)
 	// TODO : Add buy logic
 	// here it should generate another buy kwikk button?
 	// probably not even possible, since kwikk takes payments in real cash
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,21 +1,82 @@
 import {Form} from "@remix-run/react"
+//import {redirect} from "@remix-run/node"
+import {useNavigate,redirect} from "react-router-dom"
+import React from "react"
 
 export async function action({request}) {
-	return new Response("ok", {
+	const body = await request.formData()
+
+	const res = await fetch("http://localhost:3003/auth/login", {
+		method: "POST",
+		body:JSON.stringify({
+			username: body.get("username"),
+			password: body.get("password")
+		}),
 		headers: {
-			"Set-Cookie": "session=123"
-			}
-		})
+			"Content-Type": "application/json"
+		}
+	})
+	if(!res.ok){
+		return new Response(
+			new Blob(
+				[JSON.stringify({error:"Login failed"})],
+				{type:"application/json"}
+			),
+			{status:401}
+		)
+	}
+
+	const json = await res.json()
+
+	return redirect("/", {
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: res.body
+	})
 }
 
 export default function Login() {
+	const [user, setUser] = React.useState("")
+	const [message, setMessage] = React.useState("user")
+	const [password, setPassword] = React.useState("password")
+
+	const navigate = useNavigate()
+
+	async function login(){
+		const res = await fetch("http://localhost:3003/auth/login", {
+			method: "POST",
+			body:JSON.stringify({
+				username: user,
+				password: password
+			}),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+
+		if(res.ok){
+			const json = await res.json()
+			if(!json.access_token){
+				throw new Error("No access token")
+			}
+			window.localStorage.setItem("access_token", json.access_token)
+			document.cookie = `access_token=${json.access_token}`
+			//return redirect("/")
+			navigate("/")
+		}
+		setMessage("Login failed")
+	}
+
 	return (
-		<Form method="post">
-			<label>Username</label>
-			<input type="text" name="username" />
-			<label>Password</label>
-			<input type="password" name="password" />
-			<button type="submit">Login</button>
-		</Form>
+			<div>
+				<h1>Login</h1>
+					<label>Username</label>
+					<input type="text" name="username" value={user} onChange={event => setUser(event.target.value)}/>
+					<label>Password</label>
+					<input type="password" name="password" value={password} onChange={event=>setPassword(event.target.value)}/>
+					<button onClick={login}>Login</button>
+				<p>{message}</p>
+			</div>
 	)
 }
